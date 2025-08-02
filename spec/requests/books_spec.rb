004414f6917df08cecee7dbdfdf294d799a00227
Saranya Selvaraj <saranya.professional7@gmail.com>
2025-08-02 23:15:37 +1000

    add rspec for post and delete

--- spec/requests/books_spec.rb
+++ spec/requests/books_spec.rb
@@ -1,12 +1,33 @@
 require 'rails_helper'
 
 describe 'Book API', type: :request do
-  it 'return all books' do
-    FactoryBot.create(:book, title: '1984', author: 'George Orwell')
-    FactoryBot.create(:book, title: 'The Time Machine', author: 'H.G.Wells')
-    get '/api/v1/books'
+  describe 'GET /books' do
+    it 'return all books' do
+      FactoryBot.create(:book, title: '1984', author: 'George Orwell')
+      FactoryBot.create(:book, title: 'The Time Machine', author: 'H.G.Wells')
+      get '/api/v1/books'
 
-    expect(response).to have_http_status(:success)
-    expect(JSON.parse(response.body).size).to eq(2)
+      expect(response).to have_http_status(:success)
+      expect(JSON.parse(response.body).size).to eq(2)
+    end
+  end
+
+  describe 'POST /books' do
+    it 'create a new book' do
+      expect {
+      post '/api/v1/books', params: { book: {title: 'The Martian', author: 'Andy Weir'}}
+    }.to change {Book.count}.from(0).to(1)
+      expect(response).to have_http_status(:created)
+    end
+  end
+
+  describe 'DELETE /books/:id' do
+    it 'deletes a book' do
+      FactoryBot.create(:book, title: '1984', author: 'George Orwell')
+      
+      delete '/api/v1/books/1'
+
+      expect(response).to have_http_status(:no_content)
+    end
   end
 end
